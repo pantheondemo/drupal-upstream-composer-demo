afe06128fbd2ecc0554c2f6c346e644a4a27a80e
Matt Cheney <matt@getpantheon.com>
2021-02-25 15:28:04 -0600

    Adding Cache Clear to Drush Site Install

--- web/private/scripts/drush_site_install/drush_site_install.php
+++ web/private/scripts/drush_site_install/drush_site_install.php
@@ -3,4 +3,7 @@
 echo "Installing the site...\n";
 passthru('drush site-install demo_umami --site-name=Demo');
 echo "Site installation complete.\n";
-
+//Clear all cache
+echo "Rebuilding cache.\n";
+passthru('drush cr');
+echo "Rebuilding cache complete.\n";
