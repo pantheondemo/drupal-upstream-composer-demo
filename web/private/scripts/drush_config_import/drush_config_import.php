dfeffd13e2333abfb1dcdbe9d58429382639d1cb
Matt Cheney <matt@getpantheon.com>
2021-06-11 13:39:50 -0500

    Removing Config Import
    Until we get a config export we want to use

--- web/private/scripts/drush_config_import/drush_config_import.php
+++ web/private/scripts/drush_config_import/drush_config_import.php
@@ -1,10 +1,10 @@
 <?php
 // Import all config changes.
 echo "Importing configuration from yml files...\n";
-passthru('drush config-import -y');
+// passthru('drush config-import -y');
 echo "Import of configuration complete.\n";
 //Clear all cache
 echo "Rebuilding cache.\n";
-passthru('drush cr');
+// passthru('drush cr');
 echo "Rebuilding cache complete.\n";
 
